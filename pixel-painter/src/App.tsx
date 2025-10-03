e2800006e413fd1e615b3fe898152af16674e27d
kzha05 <diya.jawalapersad@gmail.com>
2025-10-03 11:59:21 +0200

    made switch class colour function

--- pixel-painter/src/App.tsx
+++ pixel-painter/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -23,13 +22,65 @@ function App() {
 		console.log('clicked yellow');
 	};
 
+	const clickRed = () => {
+		const element = document.getElementById("pixelCanvas");
+		element.classList.remove("white");
+		element.classList.remove("blue");
+		element.classList.remove("purple");
+		element.classList.remove("green");
+		element.classList.remove("yellow");
+		element.classList.add("red");
+	};
+
+	const clickBlue = () => {
+		const element = document.getElementById("pixelCanvas");
+		element.classList.remove("white");
+		element.classList.remove("red");
+		element.classList.remove("purple");
+		element.classList.remove("green");
+		element.classList.remove("yellow");
+		element.classList.add("blue");
+	};
+
+	const clickPurple = () => {
+		const element = document.getElementById("pixelCanvas");
+		element.classList.remove("white");
+		element.classList.remove("red");
+		element.classList.remove("blue");
+		element.classList.remove("green");
+		element.classList.remove("yellow");
+		element.classList.add("purple");
+	};
+
+	const clickGreen = () => {
+		const element = document.getElementById("pixelCanvas");
+		element.classList.remove("white");
+		element.classList.remove("red");
+		element.classList.remove("blue");
+		element.classList.remove("purple");
+		element.classList.remove("yellow");
+		element.classList.add("green");
+	};
+
+	const clickYellow = () => {
+		const element = document.getElementById("pixelCanvas");
+		element.classList.remove("white");
+		element.classList.remove("red");
+		element.classList.remove("blue");
+		element.classList.remove("purple");
+		element.classList.remove("green");
+		element.classList.add("yellow");
+	};
+
 	return (
 		<>
-			<button style={{background: "red"}} onClick={ red } id="pixelCanvas"></button>
-			<button style={{background: "blue"}} onClick={ blue } id="pixelCanvas"></button>
-			<button style={{background: "purple"}} onClick={ purple } id="pixelCanvas"></button>
-			<button style={{background: "green"}} onClick={ green } id="pixelCanvas"></button>
-			<button style={{background: "yellow"}} onClick={ yellow } id="pixelCanvas"></button>
+			<button style={{background: "red"}} onClick={ clickRed } id="pixelCanvasColour"></button>
+			<button style={{background: "blue"}} onClick={ clickBlue } id="pixelCanvasColour"></button>
+			<button style={{background: "purple"}} onClick={ clickPurple } id="pixelCanvasColour"></button>
+			<button style={{background: "green"}} onClick={ clickGreen } id="pixelCanvasColour"></button>
+			<button style={{background: "yellow"}} onClick={ clickYellow } id="pixelCanvasColour"></button>
+
+			<div id="pixelCanvas" className="white="></div>
 		</>
 	);
 }
