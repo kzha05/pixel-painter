ee9b056171e926839241e84ab30af50742462ad2
kzha05 <diya.jawalapersad@gmail.com>
2025-10-13 15:54:56 +0200

    cleaned up code and made buttons function better

--- pixel-painter/src/App.tsx
+++ pixel-painter/src/App.tsx
@@ -1,86 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-	const red = () => {
-		console.log('clicked red');
-	};
+	const [color,setColor] = useState<string>("white");
 
-	const blue = () => {
-		console.log('clicked blue');
-	};
-
-	const purple = () => {
-		console.log('clicked purple');
-	};
-
-	const green = () => {
-		console.log('clicked green');
-	};
-
-	const yellow = () => {
-		console.log('clicked yellow');
-	};
-
-	const clickRed = () => {
-		const element = document.getElementById("pixelCanvas");
-		element.classList.remove("white");
-		element.classList.remove("blue");
-		element.classList.remove("purple");
-		element.classList.remove("green");
-		element.classList.remove("yellow");
-		element.classList.add("red");
-	};
-
-	const clickBlue = () => {
-		const element = document.getElementById("pixelCanvas");
-		element.classList.remove("white");
-		element.classList.remove("red");
-		element.classList.remove("purple");
-		element.classList.remove("green");
-		element.classList.remove("yellow");
-		element.classList.add("blue");
-	};
-
-	const clickPurple = () => {
-		const element = document.getElementById("pixelCanvas");
-		element.classList.remove("white");
-		element.classList.remove("red");
-		element.classList.remove("blue");
-		element.classList.remove("green");
-		element.classList.remove("yellow");
-		element.classList.add("purple");
-	};
-
-	const clickGreen = () => {
-		const element = document.getElementById("pixelCanvas");
-		element.classList.remove("white");
-		element.classList.remove("red");
-		element.classList.remove("blue");
-		element.classList.remove("purple");
-		element.classList.remove("yellow");
-		element.classList.add("green");
-	};
-
-	const clickYellow = () => {
-		const element = document.getElementById("pixelCanvas");
-		element.classList.remove("white");
-		element.classList.remove("red");
-		element.classList.remove("blue");
-		element.classList.remove("purple");
-		element.classList.remove("green");
-		element.classList.add("yellow");
-	};
+	const [currentColor,setCurrentColor] = useState<string>("white");
 
 	return (
 		<>
-			<button style={{background: "red"}} onClick={ clickRed } id="pixelCanvasColour"></button>
-			<button style={{background: "blue"}} onClick={ clickBlue } id="pixelCanvasColour"></button>
-			<button style={{background: "purple"}} onClick={ clickPurple } id="pixelCanvasColour"></button>
-			<button style={{background: "green"}} onClick={ clickGreen } id="pixelCanvasColour"></button>
-			<button style={{background: "yellow"}} onClick={ clickYellow } id="pixelCanvasColour"></button>
+			<button style={ { background: "red" } } onClick={ () => setColor("red") } id="pixelCanvasColour"></button>
+			<button style={ { background: "blue" } } onClick={ () => setColor("blue") } id="pixelCanvasColour"></button>
+			<button style={ { background: "purple" } } onClick={ () => setColor("purple") } id="pixelCanvasColour"></button>
+			<button style={ { background: "green" } } onClick={ () => setColor("green") } id="pixelCanvasColour"></button>
+			<button style={ { background: "yellow" } } onClick={ () => setColor("yellow") } id="pixelCanvasColour"></button>
 
-			<div id="pixelCanvas" className="white="></div>
+			<div id="pixelCanvas" className={currentColor} onClick={ () => setCurrentColor(color) }></div>
+			<div id="pixelCanvas" className={currentColor} onClick={ () => setCurrentColor(color) }></div>
+			<div id="pixelCanvas" className={currentColor} onClick={ () => setCurrentColor(color) }></div>
+			<div id="pixelCanvas" className={currentColor} onClick={ () => setCurrentColor(color) }></div>
+			<div id="pixelCanvas" className={currentColor} onClick={ () => setCurrentColor(color) }></div>
+			<div id="pixelCanvas" className={currentColor} onClick={ () => setCurrentColor(color) }></div>
 		</>
 	);
 }
